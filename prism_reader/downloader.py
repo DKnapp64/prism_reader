1dec108472201fb8e110267487f6bc8f5f406f83
DKnapp64 <dknapp64@gmail.com>
2023-08-25 14:01:12 -0700

    initial test working

--- prism_reader/downloader.py
+++ prism_reader/downloader.py
@@ -1,12 +1,12 @@
 """module for the connection to the PRISM Oregon State FTP server"""
-
+import os
 from pathlib import Path
 
 import datetime as dt
 import logging
 import requests
 
-import attrs
+from attrs import define
 
 URL_DAILY = "https://ftp.prism.oregonstate.edu/daily" 
 
@@ -23,7 +23,11 @@ def is_downloadable(url: str):
     return False
 
 
-@attrs.define
+def make_file_name_from_date(param: str, date: dt.date):
+    return f"PRISM_{param}_stable_4kmD2_{date.isoformat().replace('-', '')}_bil.zip"
+
+
+@define
 class Downloader:
     """A simple downloader"""
     url: str = URL_DAILY
@@ -32,7 +36,7 @@ class Downloader:
         """Function to download all data for a given date"""
         parameters = ["ppt", "tdmean", "tmax", "tmean", "tmin", "vpdmax", "vpdmin"]
         for param in parameters:
-            file_name = f"PRISM_{param}_stable_4kmD2_{date.isoformat().replace('-', '')}_bil.zip"
+            file_name = make_file_name_from_date("ppt", dt.date(2022, 8, 1))
             file_url = f"{URL_DAILY}/{param}/{date.year}/{file_name}"
             if not is_downloadable(file_url):
                 logger.info("%s is not downloadable", file_url)
@@ -40,4 +44,5 @@ class Downloader:
             r = requests.get(file_url, allow_redirects=True, timeout=10)
             with open(file_name, 'wb') as fp:
                 fp.write(r.content)
+            os.remove(file_name) 
             return(Path(file_name))
--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -5,18 +5,25 @@ import os
 from pathlib import Path
 
 from unittest import mock
-from prism_reader.downloader import Downloader
+from prism_reader.downloader import Downloader, make_file_name_from_date
 
 
-def mock_requests_get() -> bytes:
+def mock_requests_get(date: dt.date) -> bytes:
     """mock of the response for requests.get to test downloader"""
-    dir_for_test_data = os.path.dirname(os.path.abspath(__file__)).join("data")
-    test_file = dir_for_test_data.join("test_file.zip")
-    return open(test_file, "rb").read()
+    dir_for_test_data = Path(f"{os.path.dirname(os.path.abspath(__file__))}/data")
+    test_file_name = f"PRISM_ppt_stable_4kmD2_{date.isoformat().replace('-', '')}_bil.zip"
+    test_file = dir_for_test_data.joinpath(test_file_name)
+    file_content = open(test_file, "rb").read()
+    return mock.MagicMock(
+        status_code=200,
+        headers={"Content-Type":"application/zip"},
+        content=file_content,
+    )
 
-@mock.patch("requests.get", return_value=mock_requests_get())
+
 def test_downloader():
     """test downloader"""
-    downloader = Downloader()
-    local_file = downloader.download_date(dt.date(2023, 8, 1))
-    assert local_file == Path("test_file.zip")
+    with mock.patch("requests.get", return_value=mock_requests_get(dt.date(2022, 8, 1))):
+        downloader = Downloader()
+        local_file = downloader.download_date(dt.date(2022, 8, 1))
+        assert local_file == Path(make_file_name_from_date("ppt", dt.date(2022, 8, 1)))
